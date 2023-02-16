22de3d19d4f2dd5be4f337cb0797b569002e6efe
Roman <romantimilsina@gmail.com>
2023-02-16 12:01:23 +0545

    buttons_color_change_with_image_position_set

--- slider/slider.js
+++ slider/slider.js
@@ -3,23 +3,29 @@ const right = document.querySelector('.right');
 const slider = document.querySelector('.slider');
 const image = document.querySelectorAll('.image');
 const sliderLength = image.length;
-console.log(sliderLength);
+
 let i = 1;
+
+
 
 function next(){
   slider.style.transform = `translateX(-${i*400}px)` ;
+  
   i++ ;
+  changeColor(i-1)
 }
 
 function initial(){
   slider.style.transform = `translateX(0px)` ;
   i = 1 ;
+  changeColor(i-1)
 }
 
 function previous(){
   
   slider.style.transform = `translateX(-${(i - 2)*400}px)` ;
   i = i - 1;
+  changeColor(i-1)
   
 }
 
@@ -27,6 +33,7 @@ function last(){
 const x = sliderLength -1
   slider.style.transform = `translateX(-${x* 400}px)` ;
   i = sliderLength;
+  changeColor(i-1)
 }
 
 
@@ -41,14 +48,27 @@ left.addEventListener('click', () => {
 })
 
 const bottom = document.querySelector('.bottom');
+
 image.forEach((element, index) => {
   const button = document.createElement('div');
   button.classList.add('button');
-
+  
   button.addEventListener('click', () => {
+    changeColor(index);
     slider.style.transform = `translateX(-${index*400}px)` ;
+    button.style.backgroundColor = 'white';
   })
 
   bottom.appendChild(button);
+  changeColor(0);
   
 })
+
+function changeColor(sliderNumber){
+  const allbutton = document.querySelectorAll('.button');
+  allbutton.forEach(e => {
+    e.style.backgroundColor='';
+    })
+    
+    allbutton[sliderNumber].style.backgroundColor = 'white';
+}
