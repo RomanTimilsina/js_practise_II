b4a849d8a7acf38af5ee217abeeca1465e4d5ea0
Roman <romantimilsina@gmail.com>
2023-02-18 13:52:43 +0545

    filter_JS_InJsSearchBoxAndProductAdded

---
+++ filter/filter.js
@@ -0,0 +1,72 @@
+const data = [
+  {
+    id: 1,
+    name: "Invicta Men's Pro Diver",
+    img: "https://m.media-amazon.com/images/I/71e04Q53xlL._AC_UY879_.jpg",
+    price: 74,
+    cat: "Dress",
+  },
+  {
+    id: 11,
+    name: "Invicta Men's Pro Diver 2",
+    img: "https://m.media-amazon.com/images/I/71e04Q53xlL._AC_UY879_.jpg",
+    price: 74,
+    cat: "Dress",
+  },
+  {
+    id: 2,
+    name: "Timex Men's Expedition Scout ",
+    img: "https://m.media-amazon.com/images/I/91WvnZ1g40L._AC_UY879_.jpg",
+    price: 40,
+    cat: "Sport",
+  },
+  {
+    id: 3,
+    name: "Breitling Superocean Heritage",
+    img: "https://m.media-amazon.com/images/I/61hGDiWBU8L._AC_UY879_.jpg",
+    price: 200,
+    cat: "Luxury",
+  },
+  {
+    id: 4,
+    name: "Casio Classic Resin Strap ",
+    img: "https://m.media-amazon.com/images/I/51Nk5SEBARL._AC_UY879_.jpg",
+    price: 16,
+    cat: "Sport",
+  },
+  {
+    id: 5,
+    name: "Garmin Venu Smartwatch ",
+    img: "https://m.media-amazon.com/images/I/51kyjYuOZhL._AC_SL1000_.jpg",
+    price: 74,
+    cat: "Casual",
+  },
+];
+
+const Search = document.querySelector('.search');
+const ProductContainer = document.querySelector('.products');
+
+const AddProducts = (display) => {
+  
+  ProductContainer.innerHTML = display.map(p => 
+   `
+   <div class="product">
+    <img src=${p.img} alt="">
+    <p class="name">${p.name}</p>
+    <p class="product_price">$${p.price}</p>
+  </div>
+  `
+  )
+}
+
+AddProducts(data)
+
+
+  Search.addEventListener('input', (e) => {
+     const searchedData = [];
+     data.forEach(p => 
+        p.name.includes(e.target.value) ? searchedData.push(p):null
+      )
+
+      AddProducts(searchedData)
+  })
